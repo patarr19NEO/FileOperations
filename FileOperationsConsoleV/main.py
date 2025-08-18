5818a8e2c197f9d0a8f36e5d00dfbd22f2a5ff5f
yura pataridze <yurapatarr19@yandex.ru>
2025-08-18 16:20:49 +0000

    added logic.txt, added functions and createFolder and createFile are works. main added with work witn these funcrions

--- FileOperationsConsoleV/main.py
+++ FileOperationsConsoleV/main.py
@@ -1,1 +1,43 @@
-import os, sys
+import os, sys, shutil
+
+class FileOperarions():
+    def __init__(self):
+        self.current_dir = os.getcwd()
+
+    def createFolder(self, folder_name):
+        try:
+            os.mkdir(f"{self.current_dir}/{folder_name}")
+            return f"Folder {folder_name} was succesfully created in path {self.current_dir}"
+        except Exception as e:
+            return f"Failed. Error: {e}"
+
+    def createFile(self, file_name):
+        try:
+            with open(file_name, "w") as file:
+                file.write("")
+            return f"File {file_name} was succesfully created in {self.current_dir}"
+        except Exception as e:
+            return f"Failed. Error: {e}"
+
+    def openFolder(self):
+        pass
+
+    def openFile(self):
+        pass
+
+    def copyFiles(self):
+        pass
+
+    def showOS(self):
+        pass
+
+fo = FileOperarions()
+
+if __name__ == '__main__':
+    user_input = input()
+    if user_input.startswith("fileops create -fol"):
+        _, _, _, folder_name = user_input.split()
+        fo.createFolder(folder_name)
+    elif user_input.startswith("fileops create -fil"):
+        _, _, _, file_name = user_input.split()
+        fo.createFile(file_name)
