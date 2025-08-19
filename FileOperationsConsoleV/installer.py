dad2360f33cc07c22a14183c88e9eb831e68546a
patarr19NEO <yurapatarr19@yandex.ru>
2025-08-19 21:22:53 +0300

    some security changed

--- FileOperationsConsoleV/installer.py
+++ FileOperationsConsoleV/installer.py
@@ -1,7 +1,5 @@
 import os, shutil, sys, time
 
-from PyQt5.QtSql import primaryIndex
-
 """def copy_files(path):
     try:
         shutil.copy("main.py", f"{path}\\FileOperations")
@@ -15,30 +13,38 @@ from PyQt5.QtSql import primaryIndex
         print(f"Unknow Error: {e}")"""
 
 if __name__ == '__main__':
-    print("It is a installer of FileOps SW\n")
     try:
-        os.system("python --version")
-    except:
-        print("You don't have Python. Please, install Python3.x to install FileOps")
-        time.sleep(3)
-        sys.exit(1)
-    current_directory = os.getcwd()
-    install_path = input(f"Input installation path. Or press Enter to install in your current directory[{os.path.join(current_directory, 'FileOps')}]")
-    if not install_path:
-        install_path = os.path.join(current_directory, "FileOps")
-    try:
-        os.makedirs(install_path, exist_ok=True)
-    except Exception as e:
-        print("Failed. Error: ", e)
-        sys.exit(1)
-    files_for_copy = ["main.py", "installer.py"]
-    try:
-        for file in files_for_copy:
-            if not os.path.exists(file):
-                print("Files not found.")
-                sys.exit(1)
-            else:
-                shutil.copy(file, install_path)
+        print("It is a installer of FileOps SW\n")
+        try:
+            os.system("python --version")
+        except:
+            print("You don't have Python. Please, install Python3.x to install FileOps")
+            input()
+            sys.exit(1)
+        current_directory = os.getcwd()
+        install_path = input(f"Input installation path. Or press Enter to install in your current directory[{os.path.join(current_directory, 'FileOps')}]")
+        if not install_path:
+            install_path = os.path.join(current_directory, "FileOps")
+        try:
+            os.makedirs(install_path, exist_ok=True)
+        except Exception as e:
+            print("Failed. Error: ", e)
+            input()
+            sys.exit(1)
+        files_for_copy = ["main.py", "installer.py"]
+        try:
+            for file in files_for_copy:
+                if not os.path.exists(file):
+                    print("Files not found.")
+                    sys.exit(1)
+                else:
+                    shutil.copy(file, install_path)
+        except Exception as e:
+            print(f"Failed. Error: {e}")
+            input()
+            sys.exit(1)
+        print(f"Congratulations. You've successfully installed FileOps\nNow You can start:\ncd /d {install_path}\nfileops")
     except Exception as e:
         print(f"Failed. Error: {e}")
-    print(f"Congratulations. You've successfully installed FileOps\nNow You can start:\ncd /d {install_path}\nfileops")
+        input()
+        sys.exit(1)
